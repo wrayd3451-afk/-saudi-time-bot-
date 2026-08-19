5b08ed82137dca70dca569cb010b9b0cf9f1f604
wrayd3451-afk <wrayd3451@gmail.com>
2026-08-19 08:38:53 +0300

    Update main.py

--- main.py
+++ main.py
@@ -16,3 +16,14 @@ async def ping(ctx):
     await ctx.send("Pong! 🏓")
 
 bot.run(os.environ['DISCORD_TOKEN'])
+class MyView(discord.ui.View):
+    def __init__(self):
+        super().__init__()
+
+    @discord.ui.button(label="إرسال تعميم", style=discord.ButtonStyle.primary)
+    async def button_callback(self, interaction: discord.Interaction, button: discord.ui.Button):
+        await interaction.response.send_message("تم الضغط على الزر!", ephemeral=True)
+
+@bot.command()
+async def panel(ctx):
+    await ctx.send("لوحة التحكم:", view=MyView())

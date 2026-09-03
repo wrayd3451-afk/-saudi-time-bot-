4a287a6da8dcf132da11feb6ae8e002249f940bb
wrayd3451-afk <wrayd3451@gmail.com>
2026-09-03 09:05:50 +0300

    Fix formatting and update intents initialization

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
+import os
 import discord
 from discord import app_commands
 from discord.ext import commands
 
-# إعداد الصلاحيات والبوت
-intents = discord.Intents.default()
+إعداد الصلاحيات والبوت
+intents= discord.Intents.default()
 intents.message_content = True
 bot = commands.Bot(command_prefix="!", intents=intents)
 
